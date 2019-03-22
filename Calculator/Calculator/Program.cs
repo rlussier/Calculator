f08400cd85c0031883e7dc21b2a5b362f4d226c2
Reggie <reganlussier@gmail.com>
2019-03-22 13:13:31 -0500

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+            int num1 = 0; int num2 = 0;
+
+            Console.WriteLine("Do a calculation\r");
+            Console.WriteLine("------------------\n");
+
+            Console.WriteLine("Go ahead and try it");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type another number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("\ta. Add");
+            Console.WriteLine("\tb. Subtract");
+            Console.WriteLine("\tc. Multiply");
+            Console.WriteLine("\td. Divide");
+            Console.Write("You chose: ");
+
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"{num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "b":
+                    Console.WriteLine($"{num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "c":
+                    Console.WriteLine($"{num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"{num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+
+            Console.WriteLine("Close console.");
+            Console.ReadKey();
+        }
+    }
+}
